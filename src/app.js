d81c391b88d75079e2b1f41e1fe7e57d5a256c16
Federico Peirano <fedepr2345@gmail.com>
2024-05-20 19:03:42 -0300

    Render Solutions V1.5

--- src/app.js
+++ src/app.js
@@ -50,12 +50,6 @@ apiRouter.use('/api', api.routes());
 
 app.use(apiRouter.routes());
 
-app.use(async (ctx) => {
-  if (ctx.path === '/') {
-    ctx.redirect('/api');
-  }
-});
-
 app.use((ctx) => {
   throw new NotFoundError(`The path "${ctx.request.path}" is not found`);
 });

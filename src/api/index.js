82532d0e1bec7b0bff0af18efe8bebc261505211
Federico Peirano <fedepr2345@gmail.com>
2024-05-20 18:53:30 -0300

    Render Solutions V1.4

--- src/api/index.js
+++ src/api/index.js
@@ -6,8 +6,4 @@ const router = new Router();
 
 router.use('/repositories', repositories.routes());
 
-router.get('/', async (ctx) => {
-  ctx.body = 'Bienvenido a la API';
-});
-
 export default router;
